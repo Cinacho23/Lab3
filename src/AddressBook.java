f7c5accccb47b38db06c762f30799ae4c8d9ab04
antoniocinotti <antoniocinottiballa@cmail.carleton.ca>
2023-09-22 12:26:20 -0400

    Made changes to AddressBook.java, also including additional files that could not be added in initial commit due to IDE bug.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -10,11 +10,20 @@ public class AddressBook {
         buddies.add(buddy);
     }
 
-    public void removeBuddy(BuddyInfo buddy){
+    public String removeBuddy(BuddyInfo buddy){
         for(BuddyInfo b : buddies){
             if(b.equals(buddy)){
                 buddies.remove(buddy);
+                return buddy.getName();
             }
         }
+        return "Error: Buddy not found";
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        System.out.println(addressBook.removeBuddy(buddy));
     }
 }
